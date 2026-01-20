9285f6f7723110c69402fa04bbb81135cea6404c
Vitor <vitorcostaofficial@gmail.com>
2026-01-19 23:24:29 -0300

    feat: criação do método da interface para ser implementado no service

--- src/main/java/vitor/gestaoevento/repository/EventoRepository.java
+++ src/main/java/vitor/gestaoevento/repository/EventoRepository.java
@@ -3,8 +3,12 @@ package vitor.gestaoevento.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import vitor.gestaoevento.model.Evento;
+import vitor.gestaoevento.model.StatusEvento;
+import vitor.gestaoevento.model.Usuario;
+
+import java.util.List;
 
 @Repository
 public interface EventoRepository extends JpaRepository<Evento,Long> {
-    Long id(Long id);
+    List<Evento> findByStatus(StatusEvento status);
 }
