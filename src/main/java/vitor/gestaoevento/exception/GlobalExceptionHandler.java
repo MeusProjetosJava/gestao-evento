3d8356cb70721bddb752f38485ba944c0e88d9c8
Vitor <vitorcostaofficial@gmail.com>
2026-01-21 23:49:53 -0300

    feat: criação da classe orquestradora de erro

--- src/main/java/vitor/gestaoevento/exception/GlobalExceptionHandler.java
+++ src/main/java/vitor/gestaoevento/exception/GlobalExceptionHandler.java
@@ -1,4 +1,31 @@
 package vitor.gestaoevento.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import vitor.gestaoevento.dto.ApiErrorResponse;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiErrorResponse> handleIllegalArgument(IllegalArgumentException ex) {
+        ApiErrorResponse error = new ApiErrorResponse(
+                HttpStatus.BAD_REQUEST.value(), "Erro de validação", ex.getMessage()
+        );
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiErrorResponse> handleGenericException(
+            Exception ex) {
+
+        ApiErrorResponse error = new ApiErrorResponse(
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                "Erro interno",
+                "Ocorreu um erro inesperado"
+        );
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
+    }
 }
