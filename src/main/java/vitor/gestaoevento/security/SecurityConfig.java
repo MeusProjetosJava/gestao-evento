23efa1fbcf3934be5c502b3fd2544662391eb2b0
Vitor <vitorcostaofficial@gmail.com>
2026-02-02 09:02:59 -0300

    fix: ajuste da configuração

--- src/main/java/vitor/gestaoevento/security/SecurityConfig.java
+++ src/main/java/vitor/gestaoevento/security/SecurityConfig.java
@@ -6,9 +6,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+
+import javax.crypto.spec.SecretKeySpec;
 
 @EnableMethodSecurity
 @Configuration
@@ -25,7 +29,8 @@ public class SecurityConfig {
                                 "/swagger-ui/**").permitAll()
                         .anyRequest().authenticated()
                 )
-                .httpBasic(Customizer.withDefaults());
+                .oauth2ResourceServer(oauth2 ->
+                        oauth2.jwt(Customizer.withDefaults()));
 
         return http.build();
     }
@@ -35,6 +40,17 @@ public class SecurityConfig {
             AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
     }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withSecretKey(
+                new SecretKeySpec(
+                        "chave-super-secreta-mude-depois-com-32-bytes".getBytes(),
+                        "HmacSHA256"
+                )
+        ).build();
+    }
+
 
 
     @Bean
